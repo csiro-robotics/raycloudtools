d24e3d71e2ec25c54d1446f2e0c0d1501eeeec73
Thomas Lowe <thomas.lowe@csiro.au>
2020-02-28 16:14:13 +1000

    added wrapping function

---
+++ raylib/include/rayconcavehull.h
@@ -0,0 +1,111 @@
+#pragma once
+#include "rayutils.h"
+#include <set>
+
+namespace RAY
+{
+struct ConcaveHull
+{
+  ConcaveHull(const vector<Vector3d> &points);
+
+  struct SurfaceFace
+  {
+    SurfaceFace(){ triangle = -1; }
+    int tetrahedron;
+    int triangle;
+    double curvature;
+  };
+  
+  struct Edge
+  {
+    Edge(){}
+    Edge(int v1, int v2){ vertices[0] = v1; vertices[1] = v2; hasHadFace = false; }
+    int vertices[2];
+    bool hasHadFace;
+  };
+  struct Triangle
+  {
+    Triangle()
+    {
+      vertices[0] = vertices[1] = vertices[2] = -1;
+      edges = Vector3i(-1,-1,-1);
+      isSurface = false;
+      used = false;
+    }
+    bool valid(){ return vertices[0] != -1; }
+    bool isSurface;
+    bool used;
+    Vector3i vertices;
+    Vector3i edges;
+    int tetrahedra[2];
+    SurfaceFace surfaceFaceCached;
+  };
+  struct Tetrahedron
+  {
+    Tetrahedron()
+    {
+      vertices[0] = vertices[1] = vertices[2] = vertices[3] = -1;
+      seen = false;
+      id = -1;
+    }
+    bool valid(){ return vertices[0] != -1; }
+    int vertices[4];
+    int triangles[4];
+    int neighbours[4];
+    int id;
+    bool seen;
+  };
+  bool insideTetrahedron(const Vector3d &pos, const Tetrahedron &tetra)
+  {
+    Vector3d mid(0,0,0);
+    if (tetra.vertices[0] == -1 || tetra.vertices[1] == -1 || tetra.vertices[2] == -1 || tetra.vertices[3] == -1) // an outer tetrahedron
+      return false;
+    for (int j = 0; j<4; j++)
+      mid += vertices[tetra.vertices[j]] / 4.0;
+    for (int i = 0; i<4; i++)
+    {
+      Vector3d vs[3];
+      for (int j = 0; j<3; j++)
+        vs[j] = vertices[triangles[tetra.triangles[i]].vertices[j]]; 
+      Vector3d normal = (vs[1]-vs[0]).cross(vs[2]-vs[0]);
+      if ((pos - vs[0]).dot(normal) * (mid - vs[0]).dot(normal) < 0)
+        return false;
+    }
+    return true;
+  }
+  vector<bool> vertexOnSurface;
+  vector<Vector3d> vertices;
+  vector<Edge> edges;
+
+  vector<Triangle> triangles;
+  vector<Tetrahedron> tetrahedra;
+  Vector3d centre;
+  
+  struct FaceComp 
+  {
+    bool operator() (const SurfaceFace& lhs, const SurfaceFace& rhs) const
+    {
+      if (lhs.curvature == rhs.curvature)
+      {
+        if (lhs.triangle == rhs.triangle)
+          return lhs.tetrahedron < rhs.tetrahedron;
+        return lhs.triangle < rhs.triangle;
+      }
+      return lhs.curvature < rhs.curvature;
+    }
+  };
+  set<SurfaceFace, FaceComp> surface;
+  
+  void growOutwards(const ConcaveHull::Tetrahedron &tetra, double maxCurvature);
+  void growOutwards(double maxCurvature);
+  void growInwards(double maxCurvature);
+  void growInDirection(double maxCurvature, const Vector3d &dir);
+  void growBottomUp(double maxCurvature){ growInDirection(maxCurvature, Vector3d(0,0,1)); }
+  void growTopDown(double maxCurvature){ growInDirection(maxCurvature, Vector3d(0,0,-1)); }
+
+protected:
+  void growSurface(double maxCurvature);
+  bool growFront(double maxCurvature);
+  double circumcurvature(const ConcaveHull::Tetrahedron &tetra, int triangleID);
+};
+}
---
+++ src/raywrap.cpp
@@ -0,0 +1,82 @@
+/* (c) Copyright CSIRO 2013. Author: Thomas Lowe
+   This software is provided under the terms of Schedule 1 of the license agreement between CSIRO, 3DLM and GeoSLAM.
+*/
+#include <stdio.h>
+#include <fstream>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include "rayutils.h"
+#include "rayconcavehull.h"
+#include "rayply.h"
+
+void usage(bool error=false)
+{
+  cout << "Extracts the ground surface as a mesh." << endl;
+  cout << "usage:" << endl;
+  cout << "raywrap raycloud upwards 1.0 - wraps raycloud from the bottom upwards, or: downwards, inwards, outwards" << endl;
+  cout << "                               the 1.0 is the maximum curvature to bend to" << endl;
+  cout << "--fast                       - a fast method that doesn't account for overhangs." << endl;
+  exit(error);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 4 || argc>5)
+    usage();
+
+#if defined(DEBUGDRAW)
+  ros::init(argc, argv, "ConcaveHull");
+#endif
+  string file = argv[1];
+  string type = argv[2];
+  double maximumCurvature = stod(argv[3]);
+  bool overhangs = true;
+  if (argc == 5 && string(argv[4])=="--fast" or string(argv[4])=="-f")
+    overhangs = false;
+    
+  Cloud cloud;
+  cloud.load(file);
+
+  ConcaveHull concaveHull(cloud.ends);
+  if (type == "inwards")
+    concaveHull.growInwards(maximumCurvature);
+  else if (type == "outwards")
+    concaveHull.growOutwards(maximumCurvature);
+  else if (type == "upwards")
+    concaveHull.growBottomUp(maximumCurvature);
+  else if (type == "downwards")
+    concaveHull.growTopDown(maximumCurvature);
+  
+  vector<Vector3i> tris;
+  int numBads = 0;
+  for (auto &face: concaveHull.surface)
+  {
+    Vector3d centroid(0,0,0);
+    ConcaveHull::Tetrahedron &tetra = concaveHull.tetrahedra[face.tetrahedron];
+    Vector3i triVerts = concaveHull.triangles[face.triangle].vertices;
+    if (triVerts[0] == -1)
+      cout << "bad vertices in the surface" << endl;
+    if (tetra.vertices[0] != -1)
+    {
+      for (int i = 0; i<4; i++)
+        centroid += concaveHull.vertices[tetra.vertices[i]] / 4.0;
+      Vector3d vs[3];
+      for (int i = 0; i<3; i++)
+        vs[i] = concaveHull.vertices[triVerts[i]];
+      Vector3d normal = (vs[2]-vs[0]).cross(vs[1]-vs[0]);
+      if ((centroid - vs[0]).dot(normal) < 0.0)
+        swap(triVerts[1], triVerts[2]);
+    }
+    else
+      numBads++;
+    tris.push_back(triVerts);
+  }
+  if (numBads > 0)
+    cout << "number of surfaces that didn't have enough information to orient: " << numBads << endl;
+
+  writePlyMesh(file + "_mesh.ply", concaveHull.vertices, tris, true);
+  cout << "Completed, output: " << file << "_mesh.ply" << endl;
+}
+
+
