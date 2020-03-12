b1f10c84aa50da89f7a7b0596abd9d0abc08867b
Thomas Lowe <thomas.lowe@csiro.au>
2020-03-12 11:09:51 +1000

    supports concatenation in combine

--- src/raycombine.cpp
+++ src/raycombine.cpp
@@ -15,6 +15,8 @@ void usage(bool error=false)
   cout << "Combines multiple ray clouds as a union of the volumes. Outputs the combined cloud and the residual cloud of differences." << endl;
   cout << "usage:" << endl;
   cout << "raycombine raycloud1 raycloud2 ... raycloudN - combines into one cloud" << endl;
+  cout << "                               --intersect - combines as an intersection rather than union." << endl;
+  cout << "                               --concat    - combines as a simple concatenation, with all rays remaining." << endl;
   exit(error);
 }
 
@@ -26,10 +28,16 @@ int main(int argc, char *argv[])
   vector<string> files;
   int numFiles = argc-1;
   bool maximal = false;
-  if (string(argv[argc-1]) == "--max" || string(argv[argc-1]) == "-m")
+  bool concatenate = false;
+  if (string(argv[argc-1]) == "--intersect" || string(argv[argc-1]) == "-i")
   {
     numFiles--;
     maximal = true;
+  }
+  else if (string(argv[argc-1]) == "--concatenate" || string(argv[argc-1]) == "--concat" || string(argv[argc-1]) == "-c")
+  {
+    numFiles--;
+    concatenate = true;
   }
   if (numFiles < 2)
     usage();
@@ -44,19 +52,30 @@ int main(int argc, char *argv[])
     }
   }
 
+  string fileStub = files[0];
+  if (fileStub.substr(fileStub.length()-4)==".ply")
+    fileStub = fileStub.substr(0,fileStub.length()-4);
+
   vector<Cloud> clouds(files.size());
   for (int i = 0; i<(int)files.size(); i++)
     clouds[i].load(files[i]);
 
   Cloud combined;
-  Cloud differences;
-  combined.combine(clouds, differences, maximal);
-
-  string fileStub = files[0];
-  if (fileStub.substr(fileStub.length()-4)==".ply")
-    fileStub = fileStub.substr(0,fileStub.length()-4);
-
+  if (concatenate)
+  {
+    for (auto &cloud: clouds)
+    {
+      combined.starts.insert(combined.starts.end(), cloud.starts.begin(), cloud.starts.end());
+      combined.ends.insert(combined.ends.end(), cloud.ends.begin(), cloud.ends.end());
+      combined.times.insert(combined.times.end(), cloud.times.begin(), cloud.times.end());
+    }
+  }
+  else
+  {
+    Cloud differences;
+    combined.combine(clouds, differences, maximal);
+    differences.save(fileStub + "_differences.ply");
+  }
   combined.save(fileStub + "_combined.ply");
-  differences.save(fileStub + "_differences.ply");
   return true;
 }
