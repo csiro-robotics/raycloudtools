713363790dfcfe672c35b127e6f57ef5ca6ee2d0
Lowe, Tom (Data61, Pullenvale) <Thomas.Lowe@data61.csiro.au>
2020-06-22 16:29:38 +1000

    updated rayrecover, works with rotated *and* adjusted clouds, at same time

--- raycloudtools/rayrestore/rayrestore.cpp
+++ raycloudtools/rayrestore/rayrestore.cpp
@@ -40,113 +40,114 @@ int main(int argc, char *argv[])
   std::set<Eigen::Vector3i, ray::Vector3iLess> voxel_set;
   full_decimated.decimate(voxel_width, &voxel_set); 
 
-  std::cout << "decimated size: " << full_decimated.ends.size() << " altered size: " << decimated_cloud.ends.size() << std::endl;
+  std::cout << "decimated size: " << full_decimated.ends.size() << " changed cloud size: " << decimated_cloud.ends.size() << std::endl;
 
-  // how do we do this?
-  // 1. detect alignment
-  if (full_decimated.ends.size() == decimated_cloud.ends.size()) // no points added or removed, so look for a rigid transformation
+  // This code assumes chronological ordering. If we move away from this, then 
+  // we just need to sort the clouds chronologically first
+  std::cout << "finding matching points" << std::endl;
+  std::vector<Eigen::Vector2i> pairs;
+  pairs.reserve(std::min(full_decimated.ends.size(), decimated_cloud.ends.size()));
+  std::vector<int> missings;
+  missings.reserve(decimated_cloud.ends.size());
+  int j = 0;
+  const double eps = 1e-6; // small enough to account for 200,000 rays per second, 
+                           // but large enough to ignore file format/compression errors
+  int count = 0;
+  for (int i = 0; i<(int)full_decimated.ends.size() && j<(int)decimated_cloud.times.size(); i++)
   {
-    std::cout << "looking for a Euclidean transformation" << std::endl;
-    // should we assume the point ordering is the same? I think we have to, otherwise we'd need to do a rayalign,
-    // or at least use the covariance matrix to get the rigid transformation (bad if spherical!)
-
-    // we could do a covariance matrix, if the eigenvalues are too similar, then do it based on distance weighted points, and run again?
-
-    // simplest for now is to assume that they correspond, so pick 4 points (far apart) and get the transform of best fit from that
-    // hang on, if they are spatially re-ordered, we can still correspond by time....
-    // order both by time, to get in the same ordering.... they are both decimated anyway. 
-
-    int is[3] = {0, (int)full_decimated.ends.size()/3, 2*(int)full_decimated.ends.size()/3};
-    Eigen::Vector3d fullPs[3];
-    Eigen::Vector3d decPs[3];
-    Eigen::Vector3d midFull(0,0,0), midDec(0,0,0);
-    for (int i = 0; i<3; i++)
+    while (decimated_cloud.times[j] < full_decimated.times[i]-eps && j<(int)decimated_cloud.times.size()-1)
     {
-      fullPs[i] = full_decimated.ends[is[i]];
-      decPs[i] = decimated_cloud.ends[is[i]];
-      std::cout << "full time " << i << ": " << full_decimated.times[is[i]] - 1.513e9 << ", dec time: " << decimated_cloud.times[is[i]] - 1.513e9 << std::endl;
-      midFull += fullPs[i]/3.0;
-      midDec += decPs[i]/3.0;
+      missings.push_back(j);
+      j++;
     }
-    for (int i = 1; i<3; i++)
+    if (decimated_cloud.times[j] < full_decimated.times[i] + eps)
     {
-      fullPs[i] -= fullPs[0];
-      decPs[i] -= decPs[0];
-      std::cout << "fullPs[i] size: " << fullPs[i].norm() << ", decPs[i] size: " << decPs[i].norm() << std::endl;
+      pairs.push_back(Eigen::Vector2i(i,j));
+      j++;
     }
+    else
+    {
+      Eigen::Vector3d ps = full_decimated.ends[i];
+      Eigen::Vector3i place(int(std::floor(ps[0] / voxel_width)), int(std::floor(ps[1] / voxel_width)),
+                            int(std::floor(ps[2] / voxel_width)));
+      voxel_set.erase(place);
+      count++;
+    }
+  }
+  std::cout << "number of matched pairs: " << pairs.size() << ", number of removed points: " << count << ", number added: " << missings.size() << std::endl;
+ 
+  std::cout << "looking for a Euclidean transformation" << std::endl;
+  int is[3] = {pairs[0][0], pairs[pairs.size()/3][0], pairs[2*pairs.size()/3][0]};
+  int js[3] = {pairs[0][1], pairs[pairs.size()/3][1], pairs[2*pairs.size()/3][1]};
+  Eigen::Vector3d fullPs[3];
+  Eigen::Vector3d decPs[3];
+  Eigen::Vector3d midFull(0,0,0), midDec(0,0,0);
+  for (int i = 0; i<3; i++)
+  {
+    fullPs[i] = full_decimated.ends[is[i]];
+    decPs[i] = decimated_cloud.ends[js[i]];
+    midFull += fullPs[i]/3.0;
+    midDec += decPs[i]/3.0;
+  }
+  for (int i = 1; i<3; i++)
+  {
+    fullPs[i] -= fullPs[0];
+    decPs[i] -= decPs[0];
+    if ((fullPs[i] - decPs[i]).norm() > 0.01)
+      std::cout << "warning, matched points aren't a very similar distance apart: " << fullPs[i].norm() <<
+      ", " << decPs[i].norm() << " a non-rigid transform may have been applied" << std::endl;
+  }
 
-    // how to get rotation from two triangles? do it in two stages:
-    if (abs(fullPs[1].norm() - decPs[1].norm()) > 0.01)
-      std::cout << "problem, not the same: " << fullPs[1].norm() << ", " << decPs[1].norm() << std::endl;
-    Eigen::Quaterniond quat = Eigen::Quaterniond::FromTwoVectors(fullPs[1], decPs[1]);
-    Eigen::Vector3d p1 = decPs[1].cross(decPs[2]);
-    Eigen::Vector3d p2 = (fullPs[1].cross(fullPs[2]));
-    Eigen::Quaterniond quat2 = Eigen::Quaterniond::FromTwoVectors(quat * p2, p1);
-    Eigen::Quaterniond rotation = quat2 * quat;
+  // how to get rotation from two triangles? do it in two stages:
+  Eigen::Quaterniond quat = Eigen::Quaterniond::FromTwoVectors(fullPs[1], decPs[1]);
+  Eigen::Vector3d p1 = decPs[1].cross(decPs[2]);
+  Eigen::Vector3d p2 = (fullPs[1].cross(fullPs[2]));
+  Eigen::Quaterniond quat2 = Eigen::Quaterniond::FromTwoVectors(quat * p2, p1);
+  Eigen::Quaterniond rotation = quat2 * quat;
+  Eigen::Vector3d translation = midDec - rotation * midFull;
 
-    Eigen::Vector3d translation = midDec - rotation * midFull;
+  // then for every point, we look up whether it exists in test_set, and add it if it does
+  std::cout << "applying full set of points to those found in decimated cloud" << std::endl;
+  std::vector<Eigen::Vector3d> &ps = full_cloud.ends;
+  std::vector<unsigned int> indices;
+  indices.reserve(full_cloud.ends.size());
+  for (unsigned int i = 0; i<ps.size(); i++)
+  {
+    Eigen::Vector3i place(int(std::floor(ps[i][0] / voxel_width)), int(std::floor(ps[i][1] / voxel_width)),
+                          int(std::floor(ps[i][2] / voxel_width)));
+    if (voxel_set.find(place) != voxel_set.end())
+      indices.push_back(i);
+  }
+  ray::Cloud new_cloud;
+  new_cloud.starts.reserve(indices.size());
+  new_cloud.ends.reserve(indices.size());
+  new_cloud.times.reserve(indices.size());
+  new_cloud.colours.reserve(indices.size());
+  for (auto &i: indices)
+    new_cloud.addRay(full_cloud, i);
+  
+  full_cloud = new_cloud;
+
+  double r = ray::sqr(rotation.x()) + ray::sqr(rotation.y()) + ray::sqr(rotation.z());
+  if (r > 1e-16 || translation.squaredNorm() > 1e-16)
+  {
+    std::cout << "transformation detected" << std::endl;
     std::cout << "translation: " << translation.transpose() << ", rotation: " << rotation.z() << std::endl;
     full_cloud.transform(ray::Pose(translation, rotation), decimated_cloud.times[is[0]] - full_decimated.times[is[0]]);
   }
-  else // points have been added or removed, so apply these additions and removals to the whole set of voxel points
+  else
   {
-    std::cout << "looking for additions/removals" << std::endl;
-    std::set<Eigen::Vector3i, ray::Vector3iLess> test_set;
-    // first build test_set:
-    std::vector<Eigen::Vector3d> &points = decimated_cloud.ends;
-    for (int64_t i = (int64_t)points.size()-1; i >= 0; i--)
-    {
-      Eigen::Vector3i place(int(std::floor(points[i][0] / voxel_width)), int(std::floor(points[i][1] / voxel_width)),
-                            int(std::floor(points[i][2] / voxel_width)));
-      test_set.insert(place);
-    }
-
-    // then for every point, we look up whether it exists in test_set, and add it if it does
-    std::vector<Eigen::Vector3d> &ps = full_cloud.ends;
-    std::vector<unsigned int> indices;
-    for (unsigned int i = 0; i<ps.size(); i++)
-    {
-      Eigen::Vector3i place(int(std::floor(ps[i][0] / voxel_width)), int(std::floor(ps[i][1] / voxel_width)),
-                            int(std::floor(ps[i][2] / voxel_width)));
-      if (test_set.find(place) != test_set.end())
-        indices.push_back(i);
-    }
-    std::cout << "indices found: " << indices.size() << std::endl;
-    ray::Cloud new_cloud;
-    new_cloud.starts.reserve(indices.size());
-    new_cloud.ends.reserve(indices.size());
-    new_cloud.times.reserve(indices.size());
-    new_cloud.colours.reserve(indices.size());
-    for (auto &i: indices)
-      new_cloud.addRay(full_cloud, i);
-    
-    #define SUPPORT_ADDED_RAYS
-    #if defined SUPPORT_ADDED_RAYS
-    // what about additional points? can we make it work with a raycombine??
-    std::vector<Eigen::Vector3d> &qs = decimated_cloud.ends;
-    std::vector<unsigned int> indices2;
-    for (unsigned int i = 0; i<qs.size(); i++)
-    {
-      Eigen::Vector3i place(int(std::floor(qs[i][0] / voxel_width)), int(std::floor(qs[i][1] / voxel_width)),
-                            int(std::floor(qs[i][2] / voxel_width)));
-      if (voxel_set.find(place) == voxel_set.end())
-        indices2.push_back(i);
-    }
-    std::cout << "added " << indices2.size() << " extra points that are in the altered cloud" << std::endl;
-    new_cloud.starts.reserve(indices.size() + indices2.size());
-    new_cloud.ends.reserve(indices.size() + indices2.size());
-    new_cloud.times.reserve(indices.size() + indices2.size());
-    new_cloud.colours.reserve(indices.size() + indices2.size());
-    for (auto &i: indices2)
-      new_cloud.addRay(decimated_cloud, i);
-    #endif
-
-    full_cloud = new_cloud;
+    std::cout << "no detected transformation of cloud" << std::endl;
   }
-  // finally do the same for colours, but only if the colours have been edited
-  {
 
-  }
+  // now, add back in the missing ones
+  std::cout << "added " << missings.size() << " extra points that are in the altered cloud" << std::endl;
+  full_cloud.starts.reserve(full_cloud.starts.size() + missings.size());
+  full_cloud.ends.reserve(full_cloud.ends.size() + missings.size());
+  full_cloud.times.reserve(full_cloud.times.size() + missings.size());
+  full_cloud.colours.reserve(full_cloud.colours.size() + missings.size());
+  for (auto &missing: missings)
+    full_cloud.addRay(decimated_cloud, missing);
 
   std::string file_stub = full_file;
   if (full_file.substr(full_file.length() - 4) == ".ply")
