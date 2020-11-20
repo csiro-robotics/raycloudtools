d07886fbf2a159e464de60261c73fd47b0f6f60b
Lowe, Tom (Data61, Pullenvale) <Thomas.Lowe@data61.csiro.au>
2020-11-20 10:27:39 +1000

    cleaning up the code

--- raylib/rayrenderer.cpp
+++ raylib/rayrenderer.cpp
@@ -7,61 +7,13 @@
 #include "raycloud.h"
 #include "rayply.h"
 #include "rayparse.h"
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "imagewrite.h"
 
 #define DENSITY_MIN_RAYS 10 // larger is more accurate but more blurred. 0 for no adaptive blending
 
 namespace ray
 {
-/// This is used for estimating the per-voxel density of the ray cloud
-struct DensityGrid
-{
-  static const int minVoxelHits = 2;
-  static constexpr double sphericalDistributionScale = 2.0; // average area scale due to a spherical uniform distribution of leave angles relative to the rays
   
-  void calculateDensities(const std::string &file_name);
-  void addNeighbourPriors();
-  Eigen::Vector3d getCentre(const Eigen::Vector3d &pos);
-  Eigen::Vector3d getCentre(const Eigen::Vector3i &inds);
-  int getIndex(const Eigen::Vector3d &pos);
-  int getIndex(const Eigen::Vector3i &inds);
-
-  Cuboid bounds;
-  
-  struct Voxel
-  {
-    Voxel(){ numHits = numRays = pathLength = 0.0; }
-    float numHits;
-    float numRays;
-    float pathLength;
-    inline double density() const 
-    { 
-      if (numRays <= minVoxelHits)
-        return 0.0;
-      return sphericalDistributionScale * (numRays-1.0) * numHits / (1e-10 + numRays*pathLength); 
-    } 
-    void operator +=(const Voxel &other)
-    {
-      numHits += other.numHits;
-      numRays += other.numRays; 
-      pathLength += other.pathLength; 
-    }
-    Voxel operator *(float scale)
-    {
-      Voxel voxel;
-      voxel.numHits = numHits * scale;
-      voxel.numRays = numRays * scale;
-      voxel.pathLength = pathLength * scale;
-      return voxel;
-    }
-  };
-
-  std::vector<Voxel> voxels;
-  double voxelWidth;
-  Eigen::Vector3i voxelDims;
-};
-
 // density is the probability of hitting something per metre depth
 void DensityGrid::calculateDensities(const std::string &file_name)
 {
@@ -71,13 +23,13 @@ void DensityGrid::calculateDensities(const std::string &file_name)
     {
       Eigen::Vector3d start = starts[i];
       Eigen::Vector3d end   = ends[i];
-      bounds.clipRay(start, end);
+      bounds_.clipRay(start, end);
 
       // now walk the voxels
       Eigen::Vector3d dir = end - start;
-      Eigen::Vector3d source = (start - bounds.min_bound_)/voxelWidth;
+      Eigen::Vector3d source = (start - bounds_.min_bound_)/voxel_width_;
       Eigen::Vector3d p = source;
-      Eigen::Vector3d target = (end - bounds.min_bound_)/voxelWidth;
+      Eigen::Vector3d target = (end - bounds_.min_bound_)/voxel_width_;
       double length = dir.norm();
       double maxDist = (target - source).norm();
       Eigen::Vector3i inds = p.cast<int>();
@@ -97,21 +49,18 @@ void DensityGrid::calculateDensities(const std::string &file_name)
         }
         depth += minL + 1e-9;
         inds[axis] += dir[axis] > 0 ? 1 : -1;
-        if (inds[axis] < 0 || inds[axis] >= voxelDims[axis])
+        if (inds[axis] < 0 || inds[axis] >= voxel_dims_[axis])
           break;
         p = source + depth * dir / length;
         int j = getIndex(inds);
         if (colours[i].alpha > 0 && depth > maxDist)
         {
           double d = minL + maxDist - depth;
-          voxels[j].pathLength += static_cast<float>(d*voxelWidth);
-          voxels[j].numHits++;
-          voxels[j].numRays++;
+          voxels_[j].addHitRay(static_cast<float>(d*voxel_width_));
         }
         else
         {
-          voxels[j].pathLength += static_cast<float>(minL*voxelWidth); 
-          voxels[j].numRays++;
+          voxels_[j].addMissRay(static_cast<float>(minL*voxel_width_)); 
         }
       } while (depth <= maxDist);
     }
@@ -119,83 +68,84 @@ void DensityGrid::calculateDensities(const std::string &file_name)
   readPly(file_name, true, calculate, 0);
 }
 
+// This is a form of windowed average over the Moore neighbourhood (3x3x3) window.
 void DensityGrid::addNeighbourPriors()
 {
   int X = 1;
-  int Y = voxelDims[0];
-  int Z = voxelDims[0]*voxelDims[1];
+  int Y = voxel_dims_[0];
+  int Z = voxel_dims_[0]*voxel_dims_[1];
   DensityGrid::Voxel neighbours;
   double num_hit_points = 0.0;
   double num_hit_points_unsatisfied = 0.0;
+
   // This simple 3x3x3 convolution needs to be a bit sneaky to avoid having to double the memory cost.
-  // well, not that sneaky, just output into relative cell -1,-1,-1
-  
-  for (int x = 1; x<voxelDims[0]-1; x++)
+  // well, not that sneaky, we just shift the output -1,-1,-1 for each cell
+  for (int x = 1; x<voxel_dims_[0]-1; x++)
   {
-    for (int y = 1; y<voxelDims[1]-1; y++)
+    for (int y = 1; y<voxel_dims_[1]-1; y++)
     {
-      for (int z = 1; z<voxelDims[2]-1; z++)
+      for (int z = 1; z<voxel_dims_[2]-1; z++)
       {
         int ind = getIndex(Eigen::Vector3i(x,y,z));
-        if (voxels[ind].numHits > 0)
+        if (voxels_[ind].numHits() > 0)
           num_hit_points++;
-        float needed = DENSITY_MIN_RAYS - voxels[ind].numRays;
-        DensityGrid::Voxel corner_vox = voxels[ind - X - Y - Z];
-        voxels[ind - X - Y - Z] = voxels[ind]; // move centre up to corner 
-        DensityGrid::Voxel &voxel = voxels[ind - X - Y - Z]; 
+        float needed = DENSITY_MIN_RAYS - voxels_[ind].numRays();
+        DensityGrid::Voxel corner_vox = voxels_[ind - X - Y - Z];
+        voxels_[ind - X - Y - Z] = voxels_[ind]; // move centre up to corner 
+        DensityGrid::Voxel &voxel = voxels_[ind - X - Y - Z]; 
         if (needed < 0.0)
           continue;
-        neighbours  = voxels[ind-X];
-        neighbours += voxels[ind+X];
-        neighbours += voxels[ind-Y];
-        neighbours += voxels[ind+Y];
-        neighbours += voxels[ind-Z];
-        neighbours += voxels[ind+Z];
-        if (neighbours.numRays >= needed)
+        neighbours  = voxels_[ind-X];
+        neighbours += voxels_[ind+X];
+        neighbours += voxels_[ind-Y];
+        neighbours += voxels_[ind+Y];
+        neighbours += voxels_[ind-Z];
+        neighbours += voxels_[ind+Z];
+        if (neighbours.numRays() >= needed)
         {
-          voxel += neighbours * (needed/neighbours.numRays); // add minimal amount to reach DENSITY_MIN_RAYS
+          voxel += neighbours * (needed/neighbours.numRays()); // add minimal amount to reach DENSITY_MIN_RAYS
           continue;
         }
         voxel += neighbours;
-        needed -= neighbours.numRays;
+        needed -= neighbours.numRays();
 
-        neighbours  = voxels[ind-X-Y];
-        neighbours += voxels[ind-X+Y];
-        neighbours += voxels[ind+X-Y];
-        neighbours += voxels[ind+X+Y];
+        neighbours  = voxels_[ind-X-Y];
+        neighbours += voxels_[ind-X+Y];
+        neighbours += voxels_[ind+X-Y];
+        neighbours += voxels_[ind+X+Y];
 
-        neighbours += voxels[ind-X-Z];
-        neighbours += voxels[ind-X+Z];
-        neighbours += voxels[ind+X-Z];
-        neighbours += voxels[ind+X+Z];
+        neighbours += voxels_[ind-X-Z];
+        neighbours += voxels_[ind-X+Z];
+        neighbours += voxels_[ind+X-Z];
+        neighbours += voxels_[ind+X+Z];
 
-        neighbours += voxels[ind-Y-Z];
-        neighbours += voxels[ind-Y+Z];
-        neighbours += voxels[ind+Y-Z];
-        neighbours += voxels[ind+Y+Z];
-        if (neighbours.numRays >= needed)
+        neighbours += voxels_[ind-Y-Z];
+        neighbours += voxels_[ind-Y+Z];
+        neighbours += voxels_[ind+Y-Z];
+        neighbours += voxels_[ind+Y+Z];
+        if (neighbours.numRays() >= needed)
         {
-          voxel += neighbours * (needed/neighbours.numRays); // add minimal amount to reach DENSITY_MIN_RAYS
+          voxel += neighbours * (needed/neighbours.numRays()); // add minimal amount to reach DENSITY_MIN_RAYS
           continue;
         }
         voxel += neighbours;
-        needed -= neighbours.numRays;
+        needed -= neighbours.numRays();
 
         neighbours  = corner_vox;          
-        neighbours += voxels[ind-X-Y+Z];          
-        neighbours += voxels[ind-X+Y-Z];          
-        neighbours += voxels[ind+X-Y-Z];          
-        neighbours += voxels[ind-X+Y+Z];          
-        neighbours += voxels[ind+X-Y+Z];          
-        neighbours += voxels[ind+X+Y-Z];          
-        neighbours += voxels[ind+X+Y+Z];     
-        if (neighbours.numRays >= needed)
+        neighbours += voxels_[ind-X-Y+Z];          
+        neighbours += voxels_[ind-X+Y-Z];          
+        neighbours += voxels_[ind+X-Y-Z];          
+        neighbours += voxels_[ind-X+Y+Z];          
+        neighbours += voxels_[ind+X-Y+Z];          
+        neighbours += voxels_[ind+X+Y-Z];          
+        neighbours += voxels_[ind+X+Y+Z];     
+        if (neighbours.numRays() >= needed)
         {
-          voxel += neighbours * (needed/neighbours.numRays); // add minimal amount to reach DENSITY_MIN_RAYS
+          voxel += neighbours * (needed/neighbours.numRays()); // add minimal amount to reach DENSITY_MIN_RAYS
           continue;
         }
         voxel += neighbours;    
-        if (voxels[ind].numHits > 0)
+        if (voxels_[ind].numHits() > 0)
           num_hit_points_unsatisfied++;
       }
     }
@@ -215,40 +165,10 @@ void DensityGrid::addNeighbourPriors()
   }
 }
 
-Eigen::Vector3d DensityGrid::getCentre(const Eigen::Vector3d &pos)
-{
-  Eigen::Vector3d source = (pos - bounds.min_bound_)/voxelWidth;
-  Eigen::Vector3i inds = source.cast<int>();
-  return getCentre(inds);
-}
-
-Eigen::Vector3d DensityGrid::getCentre(const Eigen::Vector3i &inds)
-{
-  Eigen::Vector3d res(inds[0], inds[1], inds[2]);
-  res += Eigen::Vector3d(0.5,0.5,0.5);
-  res = res*voxelWidth + bounds.min_bound_;
-  return res;  
-}
-
-int DensityGrid::getIndex(const Eigen::Vector3d &pos)
-{
-  Eigen::Vector3d source = (pos - bounds.min_bound_)/voxelWidth;
-  Eigen::Vector3i inds = source.cast<int>();
-  return getIndex(inds);
-}
-
-int DensityGrid::getIndex(const Eigen::Vector3i &inds)
-{
-  if (inds[0]>=0 && inds[0]<voxelDims[0]
-   && inds[1]>=0 && inds[1]<voxelDims[1]
-   && inds[2]>=0 && inds[2]<voxelDims[2])
-    return inds[0] + inds[1]*voxelDims[0] + inds[2] * voxelDims[0]*voxelDims[1];
-  return 0; // error value
-}
-
 bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirection view_direction, 
                  RenderStyle style, double pix_width, const std::string &image_file)                 
 {
+  // convert the view direction into useable parameters
   int axis = 0;
   if (view_direction == ViewDirection::Top)
     axis = 2;
@@ -259,6 +179,7 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
     dir = -1;
   bool flip_x = view_direction == ViewDirection::Left || view_direction == ViewDirection::Back;
   
+  // pull out the main image axes (ax1,ax2 are the horiz,vertical axes)
   Eigen::Vector3d extent = bounds.max_bound_ - bounds.min_bound_;
   int x_axes[] = {1, 0, 0};
   int y_axes[] = {2, 2, 1};
@@ -269,20 +190,20 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
   int depth  = 1 + static_cast<int>(extent[axis] / pix_width);
   std::cout << "outputting " << width << "x" << height << " image" << std::endl;
 
+  // accumulated colour buffer
   std::vector<Eigen::Vector4d> pixels(width * height); 
   memset(&pixels[0], 0, sizeof(Eigen::Vector4d) * width*height);
-  if (style == RenderStyle::Density || style == RenderStyle::Density_rgb) // special (and complicatd) case
+  // density calculation is a special case
+  if (style == RenderStyle::Density || style == RenderStyle::Density_rgb) 
   {
     Eigen::Vector3i dims = (extent/pix_width).cast<int>() + Eigen::Vector3i(1,1,1);
     #if DENSITY_MIN_RAYS > 0
     dims += Eigen::Vector3i(1,1,1); // so that we have extra space to convolve
     #endif
-    DensityGrid grid;
-    grid.bounds = bounds;
-    grid.bounds.min_bound_ -= Eigen::Vector3d(pix_width, pix_width, pix_width);
-    grid.voxelWidth = pix_width;
-    grid.voxelDims = dims;
-    grid.voxels.resize(grid.voxelDims[0]*grid.voxelDims[1]*grid.voxelDims[2]);
+    Cuboid grid_bounds = bounds;
+    grid_bounds.min_bound_ -= Eigen::Vector3d(pix_width, pix_width, pix_width);
+    DensityGrid grid(grid_bounds, pix_width, dims);
+
     grid.calculateDensities(cloud_file);
 
     #if DENSITY_MIN_RAYS > 0
@@ -300,14 +221,15 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
           ind[axis] = z;
           ind[ax1] = x;
           ind[ax2] = y;
-          total_density += grid.voxels[grid.getIndex(ind)].density();
+          total_density += grid.voxels()[grid.getIndex(ind)].density();
         }
         pixels[x + width * y] = Eigen::Vector4d(total_density, total_density, total_density, total_density);
       }
     }
   }
-  else
+  else // otherwise we use a common algorithm, specialising on render style only per-ray
   {
+    // this lambda expression lets us chunk load the ray cloud file, so we don't run out of RAM
     auto render = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends, std::vector<double> &, std::vector<RGBA> &colours)
     {
       for (size_t i = 0; i<ends.size(); i++)
@@ -320,12 +242,14 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
         Eigen::Vector3d pos = (point - bounds.min_bound_) / pix_width;
         Eigen::Vector3i p = (pos).cast<int>();
         int x = p[ax1], y = p[ax2];
-        Eigen::Vector4d &pix = pixels[x + width*y];
+        // using 4 dimensions helps us to accumulate colours in a greater variety of ways
+        Eigen::Vector4d &pix = pixels[x + width*y]; 
         switch (style)
         {
           case RenderStyle::Ends: 
           case RenderStyle::Starts: 
-            if (pos[axis]*dir > pix[3]*dir || pix[3] == 0.0)
+            // TODO: fix the == 0.0 part in future, it can cause incorrect occlusion on points with z=0 precisely
+            if (pos[axis]*dir > pix[3]*dir || pix[3] == 0.0) 
               pix = Eigen::Vector4d(col[0], col[1], col[2], pos[axis]);
             break;
           case RenderStyle::Mean: 
@@ -336,30 +260,32 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
             break;
           case RenderStyle::Rays: 
           {
-            // factor this out (same as in rayalignment.cpp)
             Eigen::Vector3d cloud_start = starts[i];
             Eigen::Vector3d cloud_end = ends[i];
-            bounds.clipRay(cloud_start, cloud_end);
-
+            // clip to within the image (since we exclude unbounded rays from the image bounds)
+            bounds.clipRay(cloud_start, cloud_end); 
             Eigen::Vector3d start = (cloud_start - bounds.min_bound_) / pix_width;
             Eigen::Vector3d end = (cloud_end - bounds.min_bound_) / pix_width;
             Eigen::Vector3d dir = cloud_end - cloud_start;
 
-            // fast line rendering requires picking the long axis to iterate along
+            // fast approximate 2D line rendering requires picking the long axis to iterate along
             bool x_long = std::abs(dir[ax1]) > std::abs(dir[ax2]);
             int axis_long   = x_long ? ax1 : ax2;
             int axis_short  = x_long ? ax2 : ax1;
             int width_long  = x_long ? 1 : width;
             int width_short = x_long ? width : 1;
 
-            double grad = dir[axis_short] / dir[axis_long];
+            double gradient = dir[axis_short] / dir[axis_long]; 
             if (dir[axis_long] < 0.0)
-              std::swap(start, end);
-            int start_long = (int)start[axis_long];
-            int end_long = (int)end[axis_long];
-            for (int l = start_long; l <= end_long; l++)
+              std::swap(start, end); // this lets us iterate from low up to high values
+            int start_long = static_cast<int>(start[axis_long]);
+            int end_long = static_cast<int>(end[axis_long]);
+            // place a pixel at the height of each midpoint (of the pixel) in the long axis
+            double start_mid_point = 0.5 + static_cast<double>(start_long);
+            double height = start[axis_short] + (start_mid_point - start[axis_long])*gradient;
+            for (int l = start_long; l <= end_long; l++, height += gradient)
             {
-              int s = static_cast<int>(start[axis_short] + ((0.5 + (double)l) - start[axis_long])*grad);
+              int s = static_cast<int>(height);
               pixels[width_long*l + width_short*s] += Eigen::Vector4d(col[0], col[1], col[2], 1.0);
             }
             break;
@@ -376,7 +302,7 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
   double max_val = 1.0;
   std::string image_ext = getFileNameExtension(image_file);
   bool is_hdr = image_ext == "hdr";
-  if (!is_hdr) // limited range, so work out a decent maximum value, I'm using mean + two standard deviations:
+  if (!is_hdr) // limited range, so work out a sensible maximum value, I'm using mean + two standard deviations:
   {
     double sum = 0.0;
     double num = 0.0;
@@ -394,10 +320,10 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
         sum_sqr += sqr(pixel[3] - mean);
     }
     double standard_deviation = std::sqrt(sum_sqr / num);
-    std::cout << "mean: " << mean << ", sd: " << standard_deviation << std::endl;
     max_val = mean + 2.0*standard_deviation;
   }
 
+  // The final pixel buffer
   std::vector<RGBA> pixel_colours;
   std::vector<float> float_pixel_colours;
   if (is_hdr)
@@ -407,21 +333,21 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
 
   for (int x = 0; x < width; x++)
   {
-    int indx = flip_x ? width - 1 - x : x;
+    int indx = flip_x ? width - 1 - x : x; // possible horizontal flip, depending on view direction
     for (int y = 0; y < height; y++)
     {
       Eigen::Vector4d colour = pixels[x + width*y];
       Eigen::Vector3d col3d(colour[0], colour[1], colour[2]);
-      uint8_t alpha = colour[3] == 0.0 ? 0 : 255;
+      uint8_t alpha = colour[3] == 0.0 ? 0 : 255; // 'punch-through' alpha
       switch (style)
       {
         case RenderStyle::Mean:
         case RenderStyle::Rays: 
-          col3d /= colour[3];
+          col3d /= colour[3]; // simple mean
           break;
         case RenderStyle::Sum: 
         case RenderStyle::Density: 
-          col3d /= max_val;
+          col3d /= max_val; // rescale to within limited colour range
           break;
         case RenderStyle::Density_rgb: 
         {
@@ -432,7 +358,7 @@ bool renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirect
             double shade = colour[0] / max_val;
             col3d = redGreenBlueGradient(shade);
             if (shade < 0.05)
-              col3d *= 20.0*shade;
+              col3d *= 20.0*shade; // this blends the lowest densities down to black
           }
           break;
         }
--- raylib/rayrenderer.h
+++ raylib/rayrenderer.h
@@ -38,5 +38,106 @@ enum class RAYLIB_EXPORT RenderStyle
 /// Render a ray cloud according to the supplies parameters
 bool RAYLIB_EXPORT renderCloud(const std::string &cloud_file, const Cuboid &bounds, ViewDirection view_direction, 
                                RenderStyle style, double pix_width, const std::string &image_file);
+
+/// This is used for estimating the per-voxel density of a ray cloud
+struct RAYLIB_EXPORT DensityGrid
+{
+  static const int min_voxel_hits = 2;
+  static constexpr double spherical_distribution_scale = 2.0; // average area scale due to a spherical uniform distribution of leave angles relative to the rays
+  
+  DensityGrid(const Cuboid &grid_bounds, double vox_width, const Eigen::Vector3i &dims) : 
+    bounds_(grid_bounds), voxel_width_(vox_width), voxel_dims_(dims) 
+  {
+    voxels_.resize(dims[0]*dims[1]*dims[2]);
+  }
+  
+  /// This specific voxel class represents a density
+  class Voxel
+  {
+  public:
+    Voxel(){ num_hits_ = num_rays_ = path_length_ = 0.0; }
+    /// return the density that the voxel represents
+    inline double density() const;
+    /// the densities can be summed element-wise
+    inline void operator +=(const Voxel &other);
+    /// the densities can be multiplied by a scalar, element-wise
+    inline Voxel operator *(float scale) const;
+    /// Add a ray which enters the voxel and hits within it. @c length is the ray length within the voxel
+    inline void addHitRay(float length);
+    /// Add a ray which enters and exits the voxel. @c length is the ray path length within the voxel
+    inline void addMissRay(float length);
+    inline const float &numHits() const { return num_hits_; }
+    inline const float &numRays() const { return num_rays_; }
+    inline const float &pathLength() const { return path_length_; }
+    
+  private:
+    float num_hits_;
+    float num_rays_;
+    float path_length_;
+  };
+
+  /// This streams in a ray cloud file, and fills in the voxel density information
+  void calculateDensities(const std::string &file_name);
+  /// To void low-ray-count voxels giving unstable density estimates, we fuse with neighbour information
+  /// up to a specified minimum number of rays. Specified in DENSITY_MIN_RAYS 
+  void addNeighbourPriors();
+  inline int getIndex(const Eigen::Vector3d &pos) const;
+  inline int getIndex(const Eigen::Vector3i &inds) const;
+  inline const std::vector<Voxel> &voxels() const { return voxels_; }
+
+private:
+  Cuboid bounds_;
+  std::vector<Voxel> voxels_;
+  double voxel_width_;
+  Eigen::Vector3i voxel_dims_;
+};
+
+// inline functions
+inline double DensityGrid::Voxel::density() const 
+{ 
+  if (num_rays_ <= min_voxel_hits)
+    return 0.0;
+  return spherical_distribution_scale * (num_rays_-1.0) * num_hits_ / (1e-10 + num_rays_*path_length_); 
+} 
+void DensityGrid::Voxel::operator +=(const DensityGrid::Voxel &other)
+{
+  num_hits_ += other.num_hits_;
+  num_rays_ += other.num_rays_; 
+  path_length_ += other.path_length_; 
+}
+DensityGrid::Voxel DensityGrid::Voxel::operator *(float scale) const
+{
+  DensityGrid::Voxel voxel;
+  voxel.num_hits_ = num_hits_ * scale;
+  voxel.num_rays_ = num_rays_ * scale;
+  voxel.path_length_ = path_length_ * scale;
+  return voxel;
+}
+void DensityGrid::Voxel::addHitRay(float length)
+{
+  path_length_ += length;
+  num_hits_++;
+  num_rays_++;
+}
+void DensityGrid::Voxel::addMissRay(float length)
+{
+  path_length_ += length;
+  num_rays_++;
+}
+int DensityGrid::getIndex(const Eigen::Vector3d &pos) const
+{
+  Eigen::Vector3d source = (pos - bounds_.min_bound_)/voxel_width_;
+  Eigen::Vector3i inds = source.cast<int>();
+  return getIndex(inds);
+}
+int DensityGrid::getIndex(const Eigen::Vector3i &inds) const
+{
+  if (inds[0]>=0 && inds[0]<voxel_dims_[0]
+   && inds[1]>=0 && inds[1]<voxel_dims_[1]
+   && inds[2]>=0 && inds[2]<voxel_dims_[2])
+    return inds[0] + inds[1]*voxel_dims_[0] + inds[2] * voxel_dims_[0]*voxel_dims_[1];
+  return 0; // error value
+}
+
 }
 #endif // RAYLIB_RAYTRAJECTORY_H
