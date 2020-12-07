0be3672609b9b4d2ddf1a0e8355654552b17a34b
Lowe, Tom (Data61, Pullenvale) <Thomas.Lowe@data61.csiro.au>
2020-12-07 17:27:45 +1000

    refactoring

--- raylib/rayaxisalign.cpp
+++ raylib/rayaxisalign.cpp
@@ -6,90 +6,76 @@
 #include "rayaxisalign.h"
 #include "rayutils.h"
 #include "raycloud.h"
+#include "raycloudwriter.h"
 #include "rayunused.h"
 #include "rayply.h"
 
 namespace ray
 {
-// A radon transform is used as follows:
-// 1. we quantise the cloud into a 2D grid of centroids, weighted by number of end points within the cell
-// 2. for each cell, render a sine wave to weights image
-// 3. find the maximum weight in the image (the new y axis) and interpolate the angle and position using its neighbours
-// 4. find the highest orthogonal weight (the new x axis) and interpolate its position
-// 5. quantise density vertically into an array to get the strongest ground height signal, interpolating the max value
-// The advantage of the radon transform is that it does not require normals (unreliable on vegetation), it can work on 
-// fairly noisy planes (such as a vineyard row), and it should parallelise well.
-bool alignCloudToAxes(const std::string &cloud_name, const std::string &aligned_file)
+namespace
 {
-  // Calculate extents:
-  Cloud::Info info;
-  if (!Cloud::getInfo(cloud_name, info))
+/// These constants are the angular and amplitude (distance from centre) resolutions for the radon transform. 
+/// Too small and the alignment is imprecise, too large and it fails to align on noisy planes (such as a crop row).
+/// We may ultimately make these a function of the cloud's size or density, but for this iteration of the axis alignment function, they are fixed.
+const int ang_res = 256; // ang_res must be divisible by 2
+const int amp_res = 256;  
+}
+
+/// the output file @c out_file is the input file @c in_file, transformed by @c pose
+bool transformCloud(const std::string &in_file, const std::string &out_file, const Pose &pose)
+{
+  CloudWriter writer;
+  if (!writer.begin(out_file))
     return false;
+  Cloud chunk;
+
+  auto transform = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends, std::vector<double> &times, std::vector<RGBA> &colours)
+  {
+    chunk.clear();
+    chunk.times = times;
+    chunk.colours = colours;
+    for (size_t i = 0; i<ends.size(); i++)
+    {
+      chunk.starts.push_back(pose * starts[i]);
+      chunk.ends.push_back(pose * ends[i]);
+    }
+    writer.writeChunk(chunk);
+  };
+
+  if (!Cloud::read(in_file, transform))
+    return false;
+
+  writer.end();
+  return true;
+}
+
+Pose estimatePose(Eigen::Vector3d position_accumulator[][amp_res], const Cloud::Info &info)
+{
   const Eigen::Vector3d &min_bound = info.rays_bound.min_bound_;
   const Eigen::Vector3d &max_bound = info.rays_bound.max_bound_;
   const Eigen::Vector3d mid = (min_bound + max_bound)/2.0;
-
-  // fill in the arrays
-  const int ang_res = 256, amp_res = 256; // ang_res must be divisible by 2
+  const double radius = 0.5 * std::sqrt(2.0) * std::max(max_bound[0]-min_bound[0], max_bound[1]-min_bound[1]);
+  const double eps = 0.0001;
   double weights[ang_res][amp_res];
   std::memset(weights, 0, sizeof(double)*ang_res*amp_res);
-  double radius = 0.5 * std::sqrt(2.0) * std::max(max_bound[0]-min_bound[0], max_bound[1]-min_bound[1]);
-  double eps = 0.0001;
-
-  // first convert the cloud into a weighted centroid field. I'm using element [2] for the weight.
-  Eigen::Vector3d ps[amp_res][amp_res];
-  std::memset(ps, 0, sizeof(Eigen::Vector3d)*amp_res*amp_res);
-  double step_x = ((double)amp_res-1.0-eps) / (max_bound[0]-min_bound[0]);
-  double step_y = ((double)amp_res-1.0-eps) / (max_bound[1]-min_bound[1]);
-  // and set up the vertical arrays too
-  double ws[amp_res]; // TODO: height is usually much less than width... more constant voxel size?
-  std::memset(ws, 0, sizeof(double)*amp_res);
-  double step_z = ((double)amp_res - 1.0 - eps) / (max_bound[2] - min_bound[2]);
-
-  auto fill_arrays = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends, std::vector<double> &times, std::vector<ray::RGBA> &colours)
-  {
-    RAYLIB_UNUSED(starts);
-    RAYLIB_UNUSED(times);
-    RAYLIB_UNUSED(colours);
-    for (size_t e = 0; e<ends.size(); e++)
-    {
-      if (colours[e].alpha == 0) // unbounded
-        continue;
-      Eigen::Vector3d index = ends[e] - min_bound;
-      index[0] *= step_x;
-      index[1] *= step_y;
-      index[2] *= step_z;
-      Eigen::Vector3d pos = ends[e] - mid;
-      pos[2] = 1.0;
-      ps[(int)index[0]][(int)index[1]] += pos;
-
-      int k = (int)index[2];
-      double blend = index[2] - (double)k;
-      ws[k] += (1.0 - blend);
-      ws[k+1] += blend;      
-    }
-  };
-  if (!Cloud::read(cloud_name, fill_arrays))
-    return false;
-
   // process the data into a Euclidean transform called pose:
   for (int ii = 0; ii<amp_res; ii++)
   {
     for (int jj = 0; jj<amp_res; jj++)
     {
-      Eigen::Vector3d &p = ps[ii][jj];
-      double w = p[2];
+      Eigen::Vector3d &accumulator = position_accumulator[ii][jj];
+      double w = accumulator[2];
       if (w == 0.0)
         continue;
-      Eigen::Vector2d pos(p[0]/w, p[1]/w);
-      double angle = atan2(pos[0], pos[1]);
-      double amp = std::sqrt(pos[0]*pos[0] + pos[1]*pos[1]) / radius;
+      Eigen::Vector2d centroid(accumulator[0]/w, accumulator[1]/w);
+      double angle = atan2(centroid[0], centroid[1]);
+      double amplitude = std::sqrt(centroid[0]*centroid[0] + centroid[1]*centroid[1]) / radius;
 
       // now draw the sine wave for this point. (should it be anti-aliased?)
       for (int i = 0; i<ang_res; i++)
       {
         double ang = kPi * (double)i/(double)ang_res;
-        double y = amp * std::sin(ang + angle);
+        double y = amplitude * std::sin(ang + angle);
         double x = ((double)amp_res-1.0-eps) * (0.5 + 0.5*y);
         int j = (int)x;
         double blend = x - (double)j;
@@ -178,21 +164,82 @@ bool alignCloudToAxes(const std::string &cloud_name, const std::string &aligned_
     if (mid_point[1] < 0.0)
       pose = Pose(Eigen::Vector3d(0,0,0), Eigen::Quaterniond(0,0,0,1)) * pose; // 180 degree yaw
   }
+  return pose;
+}
+
+// A radon transform is used as follovertical_weights:
+// 1. we quantise the cloud into a 2D grid of centroids, weighted by number of end points within the cell
+// 2. for each cell, render a sine wave to weights image
+// 3. find the maximum weight in the image (the new y axis) and interpolate the angle and position using its neighbours
+// 4. find the highest orthogonal weight (the new x axis) and interpolate its position
+// 5. quantise density vertically into an array to get the strongest ground height signal, interpolating the max value
+// The advantage of the radon transform is that it does not require normals (unreliable on vegetation), it can work on 
+// fairly noisy planes (such as a vineyard row), and it should parallelise well.
+bool alignCloudToAxes(const std::string &cloud_name, const std::string &aligned_file)
+{
+  // Calculate extents:
+  Cloud::Info info;
+  if (!Cloud::getInfo(cloud_name, info))
+    return false;
+  const Eigen::Vector3d &min_bound = info.rays_bound.min_bound_;
+  const Eigen::Vector3d &max_bound = info.rays_bound.max_bound_;
+  const Eigen::Vector3d mid = (min_bound + max_bound)/2.0;
+
+  // fill in the arrays
+  double eps = 0.0001;
+
+  // first convert the cloud into a weighted centroid field. I'm using element [2] for the weight.
+  Eigen::Vector3d position_accumulator[amp_res][amp_res];
+  std::memset(position_accumulator, 0, sizeof(Eigen::Vector3d)*amp_res*amp_res);
+  double step_x = ((double)amp_res-1.0-eps) / (max_bound[0]-min_bound[0]);
+  double step_y = ((double)amp_res-1.0-eps) / (max_bound[1]-min_bound[1]);
+  // and set up the vertical arrays too
+  double vertical_weights[amp_res]; // TODO: height is usually much less than width... more constant voxel size?
+  std::memset(vertical_weights, 0, sizeof(double)*amp_res);
+  double step_z = ((double)amp_res - 1.0 - eps) / (max_bound[2] - min_bound[2]);
+
+  auto fill_arrays = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends, std::vector<double> &times, std::vector<ray::RGBA> &colours)
+  {
+    RAYLIB_UNUSED(starts);
+    RAYLIB_UNUSED(times);
+    RAYLIB_UNUSED(colours);
+    for (size_t e = 0; e<ends.size(); e++)
+    {
+      if (colours[e].alpha == 0) // unbounded
+        continue;
+      Eigen::Vector3d index = ends[e] - min_bound;
+      index[0] *= step_x;
+      index[1] *= step_y;
+      index[2] *= step_z;
+      Eigen::Vector3d pos = ends[e] - mid;
+      pos[2] = 1.0;
+      position_accumulator[(int)index[0]][(int)index[1]] += pos;
+
+      int k = (int)index[2];
+      double blend = index[2] - (double)k;
+      vertical_weights[k] += (1.0 - blend);
+      vertical_weights[k+1] += blend;      
+    }
+  };
+  if (!Cloud::read(cloud_name, fill_arrays))
+    return false;
+
+  Pose pose = estimatePose(position_accumulator, info);
 
   // now get the vertical displacement
   int max_k = 0;
   double max_wt = 0;
   for (int k = 0; k<amp_res; k++)
   {
-    if (ws[k] > max_wt) 
+    if (vertical_weights[k] > max_wt) 
     {
-      max_wt = ws[k];
+      max_wt = vertical_weights[k];
       max_k = k;
     }
   }
-  double w0 = ws[std::max(0, max_k-1)];
-  double w1 = ws[max_k];
-  double w2 = ws[std::min(max_k+1, amp_res-1)];
+  double w0 = vertical_weights[std::max(0, max_k-1)];
+  double w1 = vertical_weights[max_k];
+  double w2 = vertical_weights[std::min(max_k+1, amp_res-1)];
   double k2 = (double)max_k+0.5 + 0.5 * (w0 - w2) / (w0 + w2 - 2.0 * w1);  // just a quadratic maximum -b/2a for heights y0,y1,y2
   double height = (k2 / step_z) + min_bound[2];
   pose.position[2] = -height;
@@ -201,32 +248,7 @@ bool alignCloudToAxes(const std::string &cloud_name, const std::string &aligned_
 
 
   // finally, transform the cloud:
-
-  std::ofstream ofs;
-  if (!ray::writePlyChunkStart(aligned_file, ofs))
-    return false;
-
-  // By maintaining these buffers below, we avoid almost all memory fragmentation  
-  ray::RayPlyBuffer buffer;
-  std::vector<Eigen::Vector3d> new_starts, new_ends;
-
-  auto transform = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends, std::vector<double> &times, std::vector<RGBA> &colours)
-  {
-    for (size_t i = 0; i<ends.size(); i++)
-    {
-      new_starts.push_back(pose * starts[i]);
-      new_ends.push_back(pose * ends[i]);
-    }
-    ray::writePlyChunk(ofs, buffer, new_starts, new_ends, times, colours);
-    new_starts.clear();
-    new_ends.clear();
-  };
-
-  if (!ray::readPly(cloud_name, true, transform, 0))
-    return false;
-
-  ray::writePlyChunkEnd(ofs);
-  return true;  
+  return (transformCloud(cloud_name, aligned_file, pose));
 }
 
 } // namespace ray
