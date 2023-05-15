fc84ce5db4cc31882e278edf9159c237cda6b48f
Thomas Lowe <thomas.lowe@csiro.au>
2023-05-15 13:53:13 +1000

    added a script to raycloudtools, for large scale tree reconstructions

---
+++ scripts/rayextract_trees_large.sh
@@ -0,0 +1,27 @@
+# grids the large raycloud file $1 (written without the .ply extension), performs rayextract trees on each grid cell, 
+# then combines the results into a single $1_trees.txt file. 
+# ./rayextract_trees_large cloudname 50
+set -x
+raysplit $1.ply grid $2,$2,0 5  # 5 m overlap to avoid edge artefacts. This should be the width of the widest tree.
+mkdir gridfiles_cloud   
+mkdir gridfiles_mesh      
+mkdir gridfiles_trees      
+mkdir gridfiles_segmented 
+
+mv $1_*.ply gridfiles_cloud
+cd gridfiles_cloud
+for f in $1_*.ply;              # a parallel for can be used here
+do
+   rayextract terrain $f
+   mv *_mesh.ply ../gridfiles_mesh
+   rayextract trees $f ../gridfiles_mesh/${f%.ply}_mesh.ply --grid_width $2
+   mv *_segmented.ply ../gridfiles_segmented
+   mv *_trees.txt ../gridfiles_trees
+done
+
+cd ../gridfiles_trees
+treecombine $1_*_trees.txt
+mv *_combined.txt ../$1_trees.txt
+cd ..
+set +x
+# output cloudname_trees.txt
