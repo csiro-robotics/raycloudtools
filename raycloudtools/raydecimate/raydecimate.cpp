a3f5e7e99a2307a10ad0271e368e92efe09216dd
Thomas Lowe <thomas.lowe@csiro.au>
2024-05-14 11:40:39 +1000

    works quite well. Use raydecimate 20 cm 64 rays for instance

--- raycloudtools/raydecimate/raydecimate.cpp
+++ raycloudtools/raydecimate/raydecimate.cpp
@@ -21,6 +21,7 @@ void usage(int exit_code = 1)
   std::cout << "usage:" << std::endl;
   std::cout << "raydecimate raycloud 3 cm   - reduces to one end point every 3 cm" << std::endl;
   std::cout << "raydecimate raycloud 4 rays - reduces to every fourth ray" << std::endl;
+  std::cout << "raydecimate raycloud 100 cm 1000 rays - A maximum of 1000 rays per metre cubed" << std::endl;
   std::cout << "raydecimate raycloud 3 cm/m - reduces to ray ends spaced 3 cm apart for each metre of their length" << std::endl;
   // clang-format off
   exit(exit_code);
@@ -45,11 +46,14 @@ public:
 int rayDecimate(int argc, char *argv[])
 {
   ray::FileArgument cloud_file;
-  ray::IntArgument num_rays(1, 100);
+  ray::IntArgument num_rays(1, 10000);
   ray::DoubleArgument vox_width(0.01, 100.0);
   ray::DoubleArgument radius_per_length(0.01, 100.0);
   ray::ValueKeyChoice quantity({ &vox_width, &num_rays, &radius_per_length }, { "cm", "rays", "cm/m" });
-  if (!ray::parseCommandLine(argc, argv, { &cloud_file, &quantity }))
+  ray::TextArgument cm("cm"), rays("rays"); 
+  bool standard_format = ray::parseCommandLine(argc, argv, { &cloud_file, &quantity });
+  bool double_format = ray::parseCommandLine(argc, argv, { &cloud_file, &vox_width, &cm, &num_rays, &rays });
+  if (!standard_format && !double_format)
     usage();
   const bool spatial_decimation = quantity.selectedKey() == "cm";
   const bool length_decimation = quantity.selectedKey() == "cm/m";
@@ -64,6 +68,8 @@ int rayDecimate(int argc, char *argv[])
   // voxel set is global, however its size is proportional to the decimated cloud size,
   // so we expect it to fit within RAM limits
   std::set<Eigen::Vector3i, ray::Vector3iLess> voxel_set;
+  std::map<Eigen::Vector3i, Eigen::Vector2i, ray::Vector3iLess> voxel_map;
+  std::vector<Eigen::Vector3i> samples;
 
   // for the length decimation case
   int min_index = -20; // about a millimetre
@@ -82,7 +88,27 @@ int rayDecimate(int argc, char *argv[])
 
   auto decimate = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends,
                       std::vector<double> &times, std::vector<ray::RGBA> &colours) {
-    if (length_decimation)
+    if (double_format)
+    {
+      double voxel_width = 0.01 * vox_width.value();
+      // firstly we store a count per cell
+      for (size_t i = 0; i<ends.size(); i++)
+      {
+        Eigen::Vector3d coords = ends[i] / voxel_width;
+        Eigen::Vector3i coordsi = Eigen::Vector3d(std::floor(coords[0]), std::floor(coords[1]), std::floor(coords[2])).cast<int>();
+        auto found = voxel_map.find(coordsi);
+        if (found == voxel_map.end())
+        {
+          voxel_map.insert(std::pair<Eigen::Vector3i, Eigen::Vector2i>(coordsi, Eigen::Vector2i(1,0)));
+          samples.push_back(coordsi);
+        } 
+        else
+        {
+          found->second[0]++;
+        }      
+      }
+    }
+    else if (length_decimation)
     {
       for (size_t i = 0; i<ends.size(); i++)
       {
@@ -145,7 +171,57 @@ int rayDecimate(int argc, char *argv[])
 
   if (!ray::Cloud::read(cloud_file.name(), decimate))
     usage();
-  if (length_decimation)
+  if (double_format)
+  {
+    double voxel_width = 0.01 * vox_width.value();
+    for (auto &pos: samples)
+    {
+      int max_num = 0;
+      for (int x = pos[0]-1; x<=pos[0]+1; x++)
+      {
+        for (int y = pos[1]-1; y<=pos[1]+1; y++)
+        {
+          for (int z = pos[2]-1; z<=pos[2]+1; z++)
+          {
+            auto found = voxel_map.find(Eigen::Vector3i(x,y,z));
+            if (found != voxel_map.end())
+              max_num = std::max(max_num, found->second[0]);  // TODO: max of neighbours, or max 2x2 of neighbours?
+          }
+        }
+      }
+      voxel_map.find(Eigen::Vector3i(pos[0],pos[1],pos[2]))->second[1] = max_num;
+    }
+
+    auto finalise = [&](std::vector<Eigen::Vector3d> &starts, std::vector<Eigen::Vector3d> &ends,
+                        std::vector<double> &times, std::vector<ray::RGBA> &colours) 
+    {
+      chunk.resize(0);
+      for (size_t i = 0; i<ends.size(); i++)
+      {
+        Eigen::Vector3d coords = ends[i] / voxel_width;
+        Eigen::Vector3i coordsi = Eigen::Vector3d(std::floor(coords[0]), std::floor(coords[1]), std::floor(coords[2])).cast<int>();
+        auto found = voxel_map.find(coordsi);
+        if (found != voxel_map.end())
+        {
+          int num = (found->second)[1];
+          double segmentation = std::max(1.0, (double)num / (double)num_rays.value());
+          int &ends_left = (found->second)[0]; 
+          if (std::fmod((double)ends_left+1.0, segmentation) <= std::fmod((double)ends_left, segmentation))
+          {
+            chunk.starts.push_back(starts[i]);
+            chunk.ends.push_back(ends[i]);
+            chunk.colours.push_back(colours[i]);
+            chunk.times.push_back(times[i]);
+          }
+          ends_left--;
+        }
+      }
+      writer.writeChunk(chunk);
+    };
+    if (!ray::Cloud::read(cloud_file.name(), finalise))
+      usage();    // now we have locations and numbers:
+  }
+  else if (length_decimation)
   { 
     std::cout << "finalising" << std::endl;
     for (auto &map: voxel_sets)
